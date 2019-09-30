bab32a8861aa087b369b8a682d862f2b1eb8aedc
Frank Greguska <fgreguska@gmail.com>
2019-09-30 16:28:35 -0700

    add popup

--- csci587hw1viz/vizfolium.py
+++ csci587hw1viz/vizfolium.py
@@ -15,17 +15,29 @@ checkins_gdf = geopandas.GeoDataFrame(
     checkins_df, geometry=geopandas.points_from_xy(checkins_df.lon, checkins_df.lat),
     crs=pyproj.CRS.from_epsg(4326))
 
-# print(checkins_gdf.sample(1000))
+print(checkins_gdf.head())
 
 
 m = folium.Map([35.55, 139.49], zoom_start=10, tiles='cartodbpositron')
 
 popups, locations = [], []
 for idx, row in checkins_gdf.iterrows():
-    locations.append([row['geometry'].y, row['geometry'].x])
-    popups.append(folium.Popup(checkins_gdf.iloc[idx, :]))
+    locations.append([row['geometry'].y, row['geometry'].x, row['userid'], idx])
 
-FastMarkerCluster(locations, popups=popups).add_to(m)
+callback = """
+function (row) {
+    var icon = L.AwesomeMarkers.icon();
+    var marker = L.marker(new L.LatLng(row[0], row[1]));
+    marker.setIcon(icon);
+    var popup = L.popup({maxWidth: '300'});
+    const display_text = {text: 'userid='+row[2]+', checkin#='+row[3]};
+    var mytext = $(`<span>${display_text.text}</span>`)[0];
+    popup.setContent(mytext);
+    marker.bindPopup(popup);
+    return marker;
+};"""
+
+FastMarkerCluster(locations, callback=callback).add_to(m)
 
 # folium.GeoJson(checkins_gdf).add_to(m)
 pathlib.Path("./results").mkdir(parents=True, exist_ok=True)
